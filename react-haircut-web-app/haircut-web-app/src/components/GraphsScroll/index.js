7296081265ed5ba9030f3bc4c89740bcc21177c2
Sher213 <alisher213@outlook.com>
2023-12-19 11:14:40 -0500

    added data vis to homepage

--- react-haircut-web-app/haircut-web-app/src/components/GraphsScroll/index.js
+++ react-haircut-web-app/haircut-web-app/src/components/GraphsScroll/index.js
@@ -1,48 +1,195 @@
 import './index.scss';
 import React from "react";
-import { Pie } from "react-chartjs-2";
+import { Pie, Bar } from "react-chartjs-2";
 import Chart from 'chart.js/auto';
 import { CategoryScale } from 'chart.js';
 import { useState } from 'react';
-import { Data } from '../../utils/Data';
+import { FaceShapes, HairTypes, FaceHeightWidth, HairCutsFrequencyWithin6MonthsMillions2020, WidthForeheadJaw } from '../../utils/Data/data.js';
 
 Chart.register(CategoryScale);
 
 const GraphsScroll = () => {
+    const [currentChartIndex, setCurrentChartIndex] = useState(0);
 
-    const [chartData, setChartData] = useState({
-        labels: Data.map((data) => data.shape),
-        datasets: [
+    const chartData = [
+        {
+            labels: FaceShapes.map((data) => data.shape),
+            datasets: [
+                {
+                    label: 'percentages ',
+                    data: FaceShapes.map((data) => data.percentage),
+                    backgroundColor: [
+                        '#FF7F50',
+                        '#F88379',
+                        '#1B5366',
+                        '#ABE2CF',
+                        '#FFB347',
+                    ],
+                    borderColor: 'black',
+                    borderWidth: 2,
+                },
+            ]
+        },
+        {
+          labels: HairTypes.map((data) => data.type),
+          datasets: [
             {
-                label: "Percentages ",
-                data: Data.map((data) => data.percentage),
+              label: 'percentages ',
+              data: HairTypes.map((data) => data.percentage),
+              backgroundColor: ['#FF7F50', '#F88379', '#1B5366'],
+              borderColor: 'black',
+              borderWidth: 2,
+            },
+          ]
+        },
+        {
+            labels: FaceHeightWidth.map((data) => data.shape),
+            datasets: [
+              {
+                label: 'ratio ',
+                data: FaceHeightWidth.map((data) => data.mean),
                 backgroundColor: [
+                '#FF7F50',
+                '#F88379',
+                '#1B5366',
+                '#ABE2CF',
+                '#FFB347'],
+                borderColor: 'black',
+                borderWidth: 2,
+              },
+            ]
+        },
+        {
+            labels: WidthForeheadJaw.map((data) => data.shape),
+            datasets: [
+                {
+                    label: 'ratio ',
+                    data: WidthForeheadJaw.map((data) => data.mean),
+                    backgroundColor: [
                     '#FF7F50',
                     '#F88379',
                     '#1B5366',
                     '#ABE2CF',
-                    '#FFB347'
-                ],
-                bordorColor: "black",
-                borderWidth: 2
-            }
-        ]
-    });
+                    '#FFB347'],
+                    borderColor: 'black',
+                    borderWidth: 2,
+                },
+            ]
+        },
+        {
+            labels: HairCutsFrequencyWithin6MonthsMillions2020.map((data) => data.times),
+            datasets: [
+                {
+                    label: 'Number of people (millions) ',
+                    data: HairCutsFrequencyWithin6MonthsMillions2020.map((data) => data.number),
+                    backgroundColor: [
+                    '#FF7F50',
+                    '#F88379',
+                    '#1B5366',
+                    '#ABE2CF'],
+                    borderColor: 'black',
+                    borderWidth: 2,
+                },
+            ]
+        }
+    ];
+
+    const handleNextChart = () => {
+        setCurrentChartIndex((prevIndex) => (prevIndex + 1) % chartData.length);
+    };
+    
+    const handlePrevChart = () => {
+        setCurrentChartIndex((prevIndex) =>
+          prevIndex === 0 ? chartData.length - 1 : prevIndex - 1
+        );
+    };
 
     return (
         <div className='chart-container'>
-            <Pie
-                data={chartData}
-                options={{
-                    plugins: {
-                        title: {
+            {chartData.map((_, index) => (
+                <div key={index} style={{ opacity: index === currentChartIndex ? 1 : 0}}>
+                    {index === 0 ? (
+                    <Pie
+                        className='chart'
+                        data={chartData[0]}
+                        options={{
+                        plugins: {
+                            title: {
                             display: true,
-                            text: "Different Face Shapes Percentages"
-                        }
-                    }
-                }}
-            />
-        </div>
+                            text: 'Different Face Shapes',
+                            },
+                        },
+                        }}
+                    />
+                    ) : index === 1 ? (
+                    <Bar
+                        className='chart'
+                        data={chartData[1]}
+                        options={{
+                        plugins: {
+                            title: {
+                            display: true,
+                            text: 'Different Hair Types',
+                            },
+                        },
+                        }}
+                    />
+                    ) : index === 2 ? (
+                    <Bar
+                        className='chart'
+                        data={chartData[2]}
+                        options={{
+                        plugins: {
+                            title: {
+                            display: true,
+                            text: 'Mean Face Height to Width Ratio of Face Shapes',
+                            },
+                        },
+                        }}
+                    />
+                    ) : index === 3 ? (
+                    <Bar
+                        className='chart'
+                        data={chartData[3]}
+                        options={{
+                        plugins: {
+                            title: {
+                            display: true,
+                            text: 'Mean Forehead to Jaw Width Ratio of Face Shapes',
+                            },
+                        },
+                        }}
+                    />
+                    ) : (
+                    <Bar
+                        className='chart'
+                        data={chartData[4]}
+                        options={{
+                        plugins: {
+                            title: {
+                            display: true,
+                            text: 'Frequency of Haircuts within 6 Months in 2020',
+                            },
+                        },
+                        }}
+                    />
+                    )}
+                </div>
+            ))}
+            <div className='navigation'>
+                <button onClick={handlePrevChart}>&lt;</button>
+                <div className='chart-circles'>
+                    {chartData.map((_, index) => (
+                    <span
+                    key={index}
+                    className={index === currentChartIndex ? 'active-circle' : 'inactive-circle'}
+                    onClick={() => setCurrentChartIndex(index)}
+                    >&#8226;</span>
+                    ))}
+                </div>
+                <button onClick={handleNextChart}>&gt;</button>
+            </div>
+      </div>
     )
 }
 
---
+++ react-haircut-web-app/haircut-web-app/src/utils/Data/data.js
@@ -0,0 +1,142 @@
+export const FaceShapes = [
+    {
+        id: 1,
+        shape: 'oval',
+        percentage: 42.97
+    },
+
+    {
+        id: 2,
+        shape: 'heart',
+        percentage: 24.06
+    },
+
+    {
+        id: 3,
+        shape: 'oblong',
+        percentage: 23.85
+    },
+
+    {
+        id: 4,
+        shape: 'round',
+        percentage: 7.76,
+    },
+
+    {
+        id: 5,
+        shape: 'square',
+        percentage: 1.36
+    }
+];
+
+export const HairTypes = [
+    {
+        id: 1,
+        type: 'straight',
+        percentage: 45
+    },
+
+    {
+        id: 2,
+        type: 'wavy',
+        percentage: 40
+    },
+
+    {
+        id: 3,
+        type: 'curly',
+        percentage: 15
+    }
+];
+
+export const HairCutsFrequencyWithin6MonthsMillions2020 = [
+    {
+        id: 1,
+        times: '4 or more',
+        number: 59.02
+    },
+
+    {
+        id: 1,
+        times: '2-3',
+        number: 76.47
+    },
+
+    {
+        id: 1,
+        times: '1',
+        number: 36.77
+    },
+
+    {
+        id: 1,
+        times: 'None',
+        number: 5.42
+    }
+]
+
+export const FaceHeightWidth = [
+    {
+        id: 1,
+        shape: 'heart',
+        mean: 1.249
+    },
+
+    {
+        id: 2,
+        shape: 'oblong',
+        mean: 1.329
+    },
+
+    {
+        id: 3,
+        shape: 'oval',
+        mean: 1.27
+    },
+
+    {
+        id: 4,
+        shape: 'round',
+        mean: 1.251
+    },
+
+    {
+        id: 5,
+        shape: 'square',
+        mean: 1.2589
+    }
+
+];
+
+export const WidthForeheadJaw = [
+    {
+        id: 1,
+        shape: 'heart',
+        mean: 1.097
+    },
+
+    {
+        id: 2,
+        shape: 'oblong',
+        mean: 1.08
+    },
+
+    {
+        id: 3,
+        shape: 'oval',
+        mean: 1.094
+    },
+
+    {
+        id: 4,
+        shape: 'round',
+        mean: 1.0796
+    },
+
+    {
+        id: 5,
+        shape: 'square',
+        mean: 1.0795
+    }
+];
